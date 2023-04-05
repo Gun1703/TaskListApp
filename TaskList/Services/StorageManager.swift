0cb017fe655a3775036f134b01aff7af37ab9f85
Gun <ggunin1992@gmail.com>
2023-04-05 10:28:46 +0300

    Test2

--- TaskList/Services/StorageManager.swift
+++ TaskList/Services/StorageManager.swift
@@ -12,7 +12,7 @@ class StorageManager {
     static let shared = StorageManager()
     
     // MARK: - Core Data stack
-    lazy var persistentContainer: NSPersistentContainer = {
+    private var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "TaskList")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -28,13 +28,36 @@ class StorageManager {
         context = persistentContainer.viewContext
     }
     
-    private func fetchData() {
+    func createTask(taskName: String, compliition: (Task) -> Void) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task",
+                                                                 in: context) else { return }
+        guard let task = NSManagedObject(entity: entityDescription,
+                                         insertInto: context) as? Task else { return }
+        task.title = taskName
+        compliition(task)
+        saveContext()
+    }
+    
+    func fetchData(complition: (Result<[Task], Error>) ->Void) {
         let fetchRequest = Task.fetchRequest()
         
         do {
-            taskList = try viewContext.fetch(fetchRequest)
-        } catch {
-            print(error)
+            let tasks = try context.fetch(fetchRequest)
+            complition(.success(tasks))
+        } catch let error{
+            complition(.failure(error))
+        }
+    }
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
 }
--- TaskList/ViewControllers/TaskListViewController.swift
+++ TaskList/ViewControllers/TaskListViewController.swift
@@ -11,9 +11,7 @@ import CoreData
 class TaskListViewController: UITableViewController {
     
     private let cellID = "task"
-    private var taskList: [Task] = []
-    
-    private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var tasks: [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +21,45 @@ class TaskListViewController: UITableViewController {
         fetchData()
     }
     
-
+//MARK: - AlertController
+    func addNewTask() {
+        showAlert(withTitle: "New Task", andMessage: "What do you want to do?")
+    }
     
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save Task", style: .default) { [weak self] _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self?.save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    
+//    MARK: - Private func
     private func save(_ taskName: String) {
-        let task = Task(context: viewContext)
-        task.title = taskName
-        taskList.append(task)
-        
-        let indexPath = IndexPath(row: taskList.count - 1, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-        
-        if viewContext.hasChanges {
-            do {
-                try viewContext.save()
-            } catch {
-                print(error)
+        StorageManager.shared.createTask(taskName: taskName) { task in
+            self.tasks.append(task)
+            self.tableView.insertRows(
+                at: [IndexPath(row: tasks.count - 1, section: 0)],
+                with: .automatic
+            )
+        }
+    }
+    
+    private func fetchData() {
+        StorageManager.shared.fetchData { result in
+            switch result {
+            case .success(let tasks):
+                self.tasks = tasks
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
     }
@@ -71,12 +93,12 @@ private extension TaskListViewController {
 // MARK: - UITableViewDataSource
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        taskList.count
+        tasks.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        let task = taskList[indexPath.row]
+        let task = tasks[indexPath.row]
         var content = cell.defaultContentConfiguration()
         content.text = task.title
         cell.contentConfiguration = content
